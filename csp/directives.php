662f08242a0966a2fb99758d48c7226913ddaee3
Kristian Goc-Benka <kristian.goc-benka@cs2.ch>
2016-10-28 14:49:01 +0200

    Google adsense directives added

--- csp/directives.php
+++ csp/directives.php
@@ -9,7 +9,7 @@
 $directives = array(
     'default-src' => array(
         "'self'",
-        //"googleads.g.doubleclick.net" -->google adsense
+        "googleads.g.doubleclick.net"
     ),
     'script-src' => array(
         "'self'",
@@ -17,10 +17,10 @@ $directives = array(
         "https://*.googleapis.com",
         "http://html5shiv.googlecode.com",
         "data: ",
-        //"pagead2.googlesyndication.com", -->google adsense
-        //"storage.googleapis.com", -->google adsense
-        //"googleads.g.doubleclick.net", -->google adsense
-        //"ajax.googleapis.com" -->google adsense
+        "pagead2.googlesyndication.com",
+        "storage.googleapis.com",
+        "googleads.g.doubleclick.net",
+        "ajax.googleapis.com"
     ),
     'style-src' => array(
         "'self'",
@@ -32,8 +32,8 @@ $directives = array(
         "'self'",
         "https://*.fbcdn.net",
         "https://upload.wikimedia.org",
-        //"storage.googleapis.com",  -->google adsense
-        //"pagead2.googlesyndication.com"  -->google adsense
+        "storage.googleapis.com",
+        "pagead2.googlesyndication.com"
     ),
     'frame-src' => array(
         "'self'"
